10387396483a5fe108aecc917e13d904068c7327
Nataliya Lisjo <natlisjo@gmail.com>
2020-09-24 09:41:03 +0200

    HomeWork_3

--- ConsoleApp_Device/GetTemperature.cs
+++ ConsoleApp_Device/GetTemperature.cs
@@ -7,6 +7,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using ClassLibrary.Models;
 
 namespace AzureFunction
 {
@@ -17,6 +18,10 @@ namespace AzureFunction
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
+
+            var temp = new TemperatureCatalog();
+
+
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             string name = req.Query["name"];
